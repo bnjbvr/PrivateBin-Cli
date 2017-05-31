3f6c43034b2b6a55687d6e0272917e3a9766d62d
Benjamin Bouvier <public@benj.me>
2017-05-31 14:22:11 +0200

    Add command line interface;

--- privatebin.js
+++ privatebin.js
@@ -15,21 +15,14 @@
 
 'use strict';
 
-var password = process.argv[2] || '',
-    privatebinHost = process.argv[3] || 'colle.delire.party',
-    privatebinProtocol = 'https',
-    privatebinPort = 443,
-    privatebinPath = '/',
-    expiration = 'never',
-    format = 'plaintext',
-    burnafterreading = 1,
-    opendiscussion = 0;
-
 var fs = require('fs');
 var querystring = require('querystring');
-var http = require(privatebinProtocol);
+var http = require('http');
+var https = require('https');
 
 var sjcl = require('sjcl');
+var args = require('optimist').argv;
+
 var RawDeflate = require('./vendor/rawdeflate-0.5').RawDeflate;
 
 function cipher(key, password, message)
@@ -76,7 +69,8 @@ function sendData(data) {
         }
     };
 
-    var request = http.request(options, function(res) {
+    var protocol = privatebinProtocol === 'http' ? http : https;
+    var request = protocol.request(options, function(res) {
         res.setEncoding('utf8');
 
         var responseString = '';
@@ -129,6 +123,61 @@ function sendData(data) {
     request.write(postData);
     request.end();
 }
+
+// main program
+
+if (args.h || args.help) {
+    console.log(process.argv[0] + ' -- a program to send paste from the command line to a privatebin server');
+    console.log('--password SOMETHING / -p SOMETHING        uses SOMETHING as the password for this paste (no password by default)');
+    console.log('--host HOST / -h HOST                      uses HOST as the private bin server host');
+    console.log('--protocol PROTOCOL                        uses PROTOCOL as the protocol to connect to the privatebin server (http or https, https by default)');
+    console.log('--port PORT / -P PORT                      uses PORT as the port to connect to the privatebin server (443 by default)');
+    console.log('--path PATH                                the path from which the privatebin program is served (/ by default)');
+    console.log('--expire EXPIRE / -e EXPIRE                EXPIRE notice for this paste (among "5min", "10min", "1hour", "1day", "1week", "1month", "1year", "never")');
+    console.log('--format FORMAT / -f FORMAT                FORMAT to use for this paste (among "plaintext (default), syntaxhighlighting, markdown")');
+    console.log('--burn / -b                                post this paste in burn-after-reading mode');
+    console.log('--opendiscussion / -o                      opens this paste to discussion');
+    process.exit(0);
+}
+
+
+var password = args.password || args.p || '';
+var privatebinHost = args.host || args.h || 'colle.delire.party';
+
+var privatebinProtocol = args.protocol || 'https';
+if (['http', 'https'].indexOf(privatebinProtocol) < 0) {
+    console.error("Protocol must be http or https, exiting.");
+    process.exit(1);
+}
+
+var privatebinPort = args.port || args.P || 443;
+try {
+    if ((privatebinPort | 0) !== privatebinPort)
+        throw new Error('not an integer');
+    privatebinPort |= 0;
+    if (privatebinPort < 0 || privatebinPort > 65536)
+        throw new Error('not a valid port number');
+} catch (err) {
+    console.error('Port must be an integer between 0 and 65536');
+    process.exit(1);
+}
+
+var privatebinPath = args.path || '/';
+
+var expiration = args.expire || args.e || 'never';
+if (["5min", "10min", "1hour", "1day", "1week", "1month", "1year", "never"].indexOf(expiration) < 0) {
+    console.error('Expiration date must be a value as described in the help.');
+    process.exit(1);
+}
+
+var format = args.format || args.f || 'plaintext';
+if (['plaintext', 'syntaxhighlighting', 'markdown'].indexOf(format) < 0) {
+    console.error('Format must be a value as described in the help.');
+    process.exit(1);
+}
+
+var burnafterreading = (args.burn || args.b) ? 1 : 0;
+var opendiscussion = (args.opendiscussion || args.o) ? 1 : 0;
 
 var data = '';
 process.stdin.on('readable', function() {
